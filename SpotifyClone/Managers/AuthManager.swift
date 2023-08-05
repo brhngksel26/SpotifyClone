0d6894dd114a1af045261dd5b144d2457d2ede92
Burhan <brhngksel@gamil.com>
2023-08-05 03:56:25 +0300

    feat: add auth set up

--- SpotifyClone/Managers/AuthManager.swift
+++ SpotifyClone/Managers/AuthManager.swift
@@ -10,7 +10,23 @@ import Foundation
 final class AuthManager{
     static let shared = AuthManager()
     
+    struct Constants {
+        static let clientID = "4c1366b54f8f433382c49182525f8e76"
+        static let clientSecret = "8049d401ea9844e8a2bb7d32d6b22e9f"
+    }
+    
     private init() {}
+    
+
+
+         
+    public var signInURL: URL? {
+        let scopes = "user-read-private"
+        let redirectURI = "https://www.google.com.tr"
+        let baseURL = "https://accounts.spotify.com/authorize"
+        let string = "\(baseURL)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        return URL(string: string)
+    }
     
     var isSignedIn: Bool {
         return false
@@ -30,5 +46,17 @@ final class AuthManager{
     
     private var shouldRefreshToken: Bool {
         return false
+    }
+    
+    public func exchangeCodeForToken(code: String, completion: @escaping ((Bool) -> Void)) {
+        
+    }
+    
+    public func refreshAccessToken(){
+        
+    }
+    
+    public func cacheToken(){
+        
     }
 }
